87c0e0b24529978b0b2b44d4b7d705930d465e95
Ttanny <revseev39@gmail.com>
2025-05-04 14:58:35 +0700

    Сделала 1 задание

--- task1.py
+++ task1.py
@@ -3,3 +3,9 @@
 # Пример ввода
 # яблоки сливы груши персики манго киви апельсины
 # 34 56 23 89 55 32 11
+string1 = input().split()
+string2 = input().split()
+dictionary = dict()
+for i in range(len(string1)):
+    dictionary[string1[i]] = int(string2[i])
+print(dictionary)
